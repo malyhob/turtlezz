732494eb25366aedef63b18ac535c4581f35492d
malyhob <80227661+malyhob@users.noreply.github.com>
2025-04-15 11:32:56 +0200

    Add files via upload

--- minr1.lua
+++ minr1.lua
@@ -11,7 +11,7 @@ local maxDug = 0
 
 -- names
 local names = http.get("https://raw.githubusercontent.com/malyhob/turtlezz/refs/heads/main/names.lua")
-local name = names[math.random(1,#names)]
+local name = names[math.random(1,table.getn(names))]
 
 os.setComputerLabel(name)
 
